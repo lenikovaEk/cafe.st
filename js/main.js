d3ca44ce6fbd921f069522358d9e06c44490d908
lenikovaEk <108451849+lenikovaEk@users.noreply.github.com>
2024-01-30 14:57:49 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function (){
+
+  $(".menu a, .go-").on("click", function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1500);
+  });
+   });
